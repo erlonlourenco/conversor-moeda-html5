4a9e75d136d55f6a84210007e97399ad95e288b4
Erlon Lourenço <erlon.abap@yahoo.com>
2019-08-24 01:25:17 +0000

    Refaturação remoção de funções globais

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-function chamarAPI(){
-	console.log("Chamando API");
-<<<<<<< HEAD
-	const endpoint = "https://api.exchangeratesapi.io/latest"; 
-=======
-	const endpoint = "api/latest";
->>>>>>> destination
-	var oParametros = {
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback );
-}
+const ovly = { };
 
-function callback(resultado){  
-	// console.log(resultado); 
-	
-	var sMoedaDestino = $("#moeda_destino").val();
-	var fValor = resultado.rates[sMoedaDestino];
-	
-	$("#valor_destino").text(fValor);
-	$("#moeda_destino_celula").text(sMoedaDestino);
+ovly.conversor = { 
+	versao: "1",
+	chamarAPI: function(){
+		console.log("Chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		// console.log(resultado);
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fValor = resultado.rates[sMoedaDestino];
+		$("#valor_destino").text(fValor);
+		$("#moeda_destino_celula").text(sMoedaDestino);
+	}
 }
